ffbb7522c1e2fd7177bb19c0ef85622dce3c9b11
Shaik Shahid Afridi <afridssh@gmail.com>
2021-12-03 16:28:07 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -4,14 +4,17 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+let count=0;
+function counts(){
+  count++;
+  const element=<p>{count}</p>;
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  
+ element, document.getElementById('root')
+);}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+setInterval(counts, 1000);
